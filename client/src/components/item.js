17ae04c65b888d43520613b28da872cada00020c
Akshit <akshitsood2013@gmail.com>
2022-05-24 17:51:52 +0530

    first commit

---
+++ client/src/components/item.js
@@ -0,0 +1,27 @@
+import {Button} from 'antd'
+import React from 'react'
+
+import {useDispatch} from 'react-redux'
+
+function Item({ item }) {
+
+  const dispatch = useDispatch()
+  function addToCart() 
+  {
+  dispatch({type:'addToCart', payload:{...item,quantity:1}})
+  
+  }
+  return (
+    <div className='item'>
+      <h4 className='name'>{item.name}</h4>
+      <img src={item.image} alt="" height='100' width='100' />
+      <h4 className='price'><b>Price: Rs {item.price} only/-</b></h4>
+      <div className="d-flex justify-content-end">
+        <Button className='onhover' onClick={() => addToCart()}> Add to Cart</Button>
+      </div>
+    </div>
+
+  )
+}
+
+export default Item
